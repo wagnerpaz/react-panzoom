344a1ac4283a4a52871ce40b511669e5901e452e
Wagner de Oliveira Paz <wagner.o.paz@gmail.com>
2021-02-07 20:05:21 -0300

    Add controls to the example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Panzoom from '@metapriori/react-panzoom';
 
 const App = () => {
+  const [zoom, setZoom] = useState(100);
   return (
-      <Panzoom style={{width: 500, height: 500}} pan={{x: 0, y: 0}} zoom={50}>
+    <>
+      <Panzoom style={{width: 500, height: 500}} pan={{x: 0, y: 0}} zoom={zoom}>
         <img alt="test" src="http://www.metapriori.com.br/wp-content/uploads/2018/10/animal-big-big-cat-247502.jpg"/>
       </Panzoom>
+      <button onClick={() => setZoom(zoom => zoom + 10)}>Increase zoom</button>
+      <button onClick={() => setZoom(zoom => zoom - 10)}>Decrease zoom</button>
+      <span>{zoom}%</span>
+    </> 
   );
 };
 
